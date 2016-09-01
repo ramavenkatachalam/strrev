7a34b2628e028b30685bde9bc68f5b4607c52794
rama <bubloo2495@gmail.com>
2016-09-01 20:49:08 +0530

    Add files via upload

---
+++ Strrev.java
@@ -0,0 +1,11 @@
+package guvi;
+
+public class Strrev 
+{public static void main(String[] args)
+{
+	String s="rama";
+	StringBuffer v=new StringBuffer(s);
+	System.out.println(v.reverse());
+}
+
+}
